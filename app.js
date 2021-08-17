243d77db4bff8a3cc6ad7c9bc6f2014a59b80202
tuananhnguyen28 <tuananhnguyenasp@gmail.com>
2021-08-17 23:20:26 +0700

    Cookie

---
+++ app.js
@@ -0,0 +1,21 @@
+// Cookie: cho phép bạn lưu trữ thông tin người dùng website vào máy tính của bạn
+
+// tạo cookie
+document.cookie = 'username=Tuan Nguyen'
+
+// // thêm thời gian hết hạn (dạng UTC)
+// const date = new Date(2021, 8, 18).toString()
+// document.cookie = `username=Nguyen Tuan; expires=${date}; path=/`
+
+// // xóa một cookie, chỉ cần set thời gian hết hạn (01 Jan 1970)
+// document.cookie = 'username=Nguyen Tuan, expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/'
+// console.log(document.cookie)
+
+/*
+  - Nếu set 1 cookie mới, tất cả các cookie cũ sẽ không bị thay thế, cookie mới sẽ được thêm vào document.cookie
+  - Cookie đc lưu theo trang web. Vd đang ở xdevclass.com thì không thể truy cập đến cookie của trang google.com được 
+  lưu trên máy bạn.
+  - Thường dùng cookie với thư viện js-cookie
+*/
+
+
