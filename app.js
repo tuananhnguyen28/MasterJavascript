41df0e6f01ffafc4dff495676350e03bb78fc029
tuananhnguyen28 <tuananhnguyenasp@gmail.com>
2021-08-18 19:47:33 +0700

    Local Storage

--- app.js
+++ app.js
@@ -23,6 +23,53 @@
   - Cookie đc lưu theo trang web. Vd đang ở xdevclass.com thì không thể truy cập đến cookie của trang google.com được 
   lưu trên máy bạn.
   - Thường dùng cookie với thư viện js-cookie
+  - Nếu không truyền giá trị của expires thì mặc định thời gian hết hạn sẽ trong session
 */
 
 
+// 2. Local Storage: tương tự Cookie, là một web API giúp bạn lưu data website vào máy tính, tồn tại mãi mãi cho đến khi bị xóa
+
+// thêm item
+localStorage.setItem('name', 'NguyenTuan')
+localStorage.setItem('age', '28')
+
+// đọc item
+const name = localStorage.getItem('name')
+console.log('localStorage for name:', name)
+
+// xóa item
+localStorage.removeItem('name')
+const nameAfterRemoving = localStorage.getItem('name')
+console.log('localStorage after removing:', nameAfterRemoving)
+
+/*
+  // xóa hết localStorage
+  localStorage.clear()
+*/
+
+// Lưu ý: khi lưu localStorage, khi dùng method 'getItem' sẽ luôn trả về kiểu string
+localStorage.setItem('gender', 1)
+console.log(typeof localStorage.getItem('gender'))
+
+/*
+  Cách chuyển từ một string -> chuỗi đặc biệt (object dạng string)
+  Mục đích để lưu vào localStorage, vì localStorage chỉ lưu data dạng string
+*/
+const profile = {
+  name: 'Tuan', 
+  age: 28,
+  gender: 'male'
+}
+
+const profileJson = JSON.stringify(profile)
+console.log(profileJson)
+console.log(typeof profileJson)
+
+// Chuyển chuỗi đặc biệt về lại object
+const profileObject = JSON.parse(profileJson)
+console.log(profileObject)
+console.log(typeof profileObject)
+
+// Lưu chuỗi 'object dạng string' vào localStorage
+// Khi click vào value trên tab Application/Local Storage có thể thấy được tất cả properties của object
+localStorage.setItem('profile', profileJson)
