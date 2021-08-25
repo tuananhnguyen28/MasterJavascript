9d52bf74783f1e9e7bdd7c0af4f9ddcac032602c
tuananhnguyen28 <tuananhnguyenasp@gmail.com>
2021-08-25 22:30:02 +0700

    Handle all columns of Interest Calculation table except Repayment Period column

--- app.js
+++ app.js
@@ -28,4 +28,50 @@ document.querySelector('form').addEventListener('submit', event => {
   document.getElementById('interest').value = interest.toLocaleString()
   document.getElementById('originalAndInterest').value = originalAndInterest.toLocaleString()
 
+  // Dùng vòng lặp for để display tương ứng với số tháng
+  for(let i = 0; i <= months; i++) {
+    if(i === 0) {
+      html = `
+        <td>${i}</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      `
+    } else if(i === months) { // tháng cuối cùng tương ứng với số tháng
+      // Gốc hàng tháng
+      const originalPerMonth = loan - Math.round(loan / months) * (months - 1)
+      // Lãi hàng tháng
+      const interestPerMonth = interest - Math.round((loan * rate) / 1200) * (months - 1)
+      // Gốc và lãi hàng tháng
+      const originalAndInterestPerMonth = originalPerMonth + interestPerMonth
+      html = `
+        <td>${i}</td>
+        <td></td>
+        <td>${originalPerMonth}</td>
+        <td>${interestPerMonth}</td>
+        <td>${originalAndInterestPerMonth}</td>
+        <td>0</td>
+      `
+    } else {  // các tháng ở giữa
+      const originalPerMonth = Math.round(loan / months)
+      const interestPerMonth = Math.round(loan * rate) / 1200
+      const originalAndInterestPerMonth = originalPerMonth + interestPerMonth
+      // Gốc còn lại
+      const remainingOriginal = loan - originalPerMonth * i
+      html = `
+        <td>${i}</td>
+        <td></td>
+        <td>${originalPerMonth}</td>
+        <td>${interestPerMonth}</td>
+        <td>${originalAndInterestPerMonth}</td>
+        <td>${remainingOriginal}</td>
+      `
+    }
+    // Load dữ liệu vào bảng
+    const tr = document.createElement('tr')
+    tr.innerHTML = html
+    document.querySelector('table tbody').appendChild(tr)
+  }
 })
