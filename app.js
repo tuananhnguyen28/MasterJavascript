f303dd180772f2f042b233158d6f690692e54100
tuananhnguyen28 <tuananhnguyenasp@gmail.com>
2021-09-21 16:13:45 +0700

    Update app javascript file to validate form correctly

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 // 1. Tạo Regex cho Email
-const emailRegex = /^[a-zA-Z\d\.\-\_]+(\+\d+)?@[a-zA-Z\d\.\-]{1,63}\.[a-zA-Z]{1,5}$/
+const REGEX_EMAIL = /^[a-zA-Z\d\.\-\_]+(\+\d+)?@[a-zA-Z\d\.\-]{1,63}\.[a-zA-Z]{1,5}$/
 const needToCheckString = 'nguyenanhtuan+01@gmail-vn.com'
 
 // 2. Tạo Regex cho Name (required, là tên người - tiếng Anh hoặc tiếng Việt không chứa số, max 50 ký tự)
-const nameRegex = /^[a-zA-Z\u00c0-\u024F\u1E00-\u1EFF]+((\s[a-zA-Z\u00C0-\u024F\u1E00-\u1EFF]+)?)+$/
+const REGEX_NAME = /^[a-zA-Z\u00C0-\u024F\u1E00-\u1EFF]+((\s[a-zA-Z\u00C0-\u024F\u1E00-\u1EFF]+)?)+$/
 const theNameNeedToCheck_1 = 'Nguyễn Anh Tuấn'
 const theNameNeedToCheck_2 = 'ab  Tuấn'
 const theNameNeedToCheck_3 = '1goodfornewsHW'
-console.log(nameRegex.test(theNameNeedToCheck_1)) // true
-console.log(nameRegex.test(theNameNeedToCheck_2)) // false
-console.log(nameRegex.test(theNameNeedToCheck_3)) // false
+console.log(REGEX_NAME.test(theNameNeedToCheck_1)) // true
+console.log(REGEX_NAME.test(theNameNeedToCheck_2)) // false
+console.log(REGEX_NAME.test(theNameNeedToCheck_3)) // false
 
 // 3. Định nghĩa một số hàm check
 const isRequired = value => value.trim() !== '' ? '' : 'Trường này thì bắt buộc'
-const isEmail = value => emailRegex.test(value) ? '' : 'Email không đúng định dạng'
-const isHomanName = value => nameRegex.test(value) ? '' : 'Tên không đúng định dạng'
+const isEmail = value => REGEX_EMAIL.test(value) ? '' : 'Email không đúng định dạng'
+const isHomanName = value => REGEX_NAME.test(value) ? '' : 'Tên không đúng định dạng'
 // Currying (High Order Function)
 const min = num => value => value >= num ? '' : `Độ dài tối thiểu là ${num}`
 const max = num => value => value <= num ? '' : `Độ dài tối đa là ${num}`
@@ -37,14 +37,14 @@ document.querySelector('form').addEventListener('submit', event => {
   const confirmedPasswordNode = document.getElementById('confirmedPassword')
   const agreeNode = document.querySelector('input#agree:checked')
 
-  const isValidForm = [
+  const errorForm = [
     isValid(emailNode.value, [isRequired, isEmail]),
-    isValid(nameNode.value, [isRequired, isHomanName, max(50)]),
+    isValid(nameNode.value, [isRequired, isHomanName]),
     isValid(genderNode.value, [isRequired]),
     isValid(nationNode ? nationNode.value : '', [isRequired]),
     isValid(passwordNode.value, [isRequired, min(8), max(20)]),
     isValid(confirmedPasswordNode.value, [isRequired, min(8), max(20)]),
     isValid(agreeNode ? agreeNode.value : '', [isRequired]),
   ]
-  console.log(isValidForm)
+  console.log(errorForm)
 })
