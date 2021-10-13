107aad219fdfa31369aff7e87c33b9bcd7233035
tuananhnguyen28 <tuananhnguyenasp@gmail.com>
2021-10-13 22:37:32 +0700

    Not just a syntactic sugar because it is easier to read

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ console.log('----------Class Syntax----------')
 console.log(user)
 
 /* 
-  Lưu ý: Khi new User('Tuan') được gọi:  và
+  Lưu ý: Khi new User('Tuan') được gọi:
     - 1 object mới được tạo
     - constructor chạy với tham số được đưa vào và gán this.name vào nó
     - Không có dấu phẩy giữa các phương thức
@@ -34,3 +34,37 @@ console.log(User.prototype.sayHi) // console.log(this.name)
 
 // Có chính xác 2 methods trong prototype
 console.log(Object.getOwnPropertyNames(User.prototype)) // constructor, sayHi
+
+
+/*
+  Không chỉ là một syntactic sugar (dễ đọc hơn)
+*/
+function Employee(name) {
+  this.name = name
+}
+Employee.prototype.sayHi = function() {
+  console.log('Employee say hi:', this.name)
+}
+const employee = new Employee('Tuan employee')
+employee.sayHi()
+console.log(employee)
+
+
+// Với một function, ta ko cần dùng từ khóa new để gọi nó <-> còn class thì ko được làm như vậy
+function House(color) {
+  this.color = color
+}
+console.log('Gọi function không dùng từ khóa new')
+House() // no error
+
+class House_Error {
+  constructor(color) {
+    this.color = color
+  }
+  sayHi() {
+    console.log(`This is ${this.color} of house.`)
+  }
+}
+// House_Error() // Error
+
+
