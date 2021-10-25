72f64bff095e12269241010a492f5c9b02092f8b
tuananhnguyen28 <tuananhnguyenasp@gmail.com>
2021-10-25 22:24:30 +0700

    Property and method of static

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MasterJavascript
+# Khóa học Master Javascript - Cơ bản đến nâng cao - Dư Thanh Được
+
+
+# Hướng Dẫn 6 | 6. OOP Javascript
+  # Lesson 9: Thuộc tính và phương thức static
+  # Facebook: https://www.facebook.com/groups/495362234941379/learning_content/?filter=185540953301646&post=1142643269588680
+  # Youtube: https://youtu.be/bNO4jWRAAIw
---
+++ app.js
@@ -0,0 +1,47 @@
+/* 1. Thuộc tính và phương thức static
+    - Với từ khoá static, ta có thể tạo các phương thức và các thuộc tính gọi trực tiếp 
+    bằng class mà không cần thông qua việc khởi tạo Object bằng new (1)
+    - Điều này tương tự với việc gán trực tiếp như thế này (2)
+    - Thường dùng static trên các phương thức thuộc về class, chứ không phải thuộc về
+    bất cứ object nào (3)
+*/
+
+// (1)
+class User1 {
+  static staticMethod() {
+    console.log(this === User1)
+  }
+}
+console.log('User1.staticMethod():')
+User1.staticMethod() // true
+
+// (2)
+class User2 {}
+User2.staticMethod = function() {
+  console.log(this === User2)
+}
+console.log('User2.staticMethod():')
+User2.staticMethod() // true
+
+// (3)
+class Article {
+  constructor(title, date) {
+    this.title = title
+    this.date = date
+  }
+  static compare(articleA, articleB) {
+    return articleA.date - articleB.date
+  }
+}
+// usage
+let articles = [
+  new Article('HTML', new Date(2021, 1, 1)),
+  new Article('CSS', new Date(2021, 0, 1)),
+  new Article('Javascript', new Date(2021, 11, 1))
+]
+articles.sort(Article.compare)
+console.log('articles.sort(...) and articles[0].title:')
+console.log(articles[0].title)  // CSS
+
+// Lưu ý: tại đây Article.compare() thực hiện so sánh các object. Nó không phải là
+// phương thức của một article, mà là của cả class
