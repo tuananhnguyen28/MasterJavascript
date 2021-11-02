a5e713d10baf3db5f5cfef6ffb4867eeb108e70a
tuananhnguyen28 <tuananhnguyenasp@gmail.com>
2021-11-02 21:28:11 +0700

    Private field / property / method

--- app.js
+++ app.js
@@ -49,4 +49,69 @@ class CoffeeMachine_2 {
   }
 }
 console.log('----- Getter/setter functions -----')
-console.log(new CoffeeMachine_2().setWaterAmount(100))
+const coffeeMachine_2 = new CoffeeMachine_2()
+// coffeeMachine_2.getWaterAmount()
+coffeeMachine_2.setWaterAmount(101.5)
+console.log(coffeeMachine_2)
+
+
+/*
+  3. Private: 
+    (1): Được bắt đầu bằng # và chỉ có thể truy cập bên trong class
+    (2): Các trường private không bị xung đột với các trường public, có thể khai báo private
+    #waterAmount và public waterAmount cùng 1 lúc
+*/
+
+// (1)
+class SoftDrink {
+
+  // Private field, Private property
+  #waterLimit = 200
+  
+  // Private method 
+  #fixWaterAmount(value) {
+    if(value < 0) return 0
+    if(value > this.#waterLimit) return this.#waterLimit
+  }
+
+  // Public method
+  setWaterAmount(value) {
+    this.#waterLimit = this.#fixWaterAmount(value)
+  }
+}
+console.log('----- Private -----')
+
+// let softDrink = new SoftDrink()
+// // Can't access privates from outside of the class
+// console.log(softDrink.#waterLimit)  // Error
+// softDrink.#fixWaterAmount(95) // Error
+
+// Invoke public property or method
+let softDrink_2 = new SoftDrink()
+softDrink_2.setWaterAmount(300)
+console.log(softDrink_2)
+
+// (2)
+class Fruit {
+
+  #waterAmount = 0
+
+  get waterAmount() {
+    return this.#waterAmount
+  }
+
+  set waterAmount(value) {
+    if(value < 0) value = 0
+    this.#waterAmount = value
+  }
+}
+let fruit = new Fruit()
+fruit.waterAmount = 100
+// console.log(fruit.#waterAmount) // Error
+
+// Nếu kế thừa từ Fruit, thì sẽ không thể truy cập vào #waterAmount
+class Other_Fruit extends Fruit {
+  method() {
+    // console.log(this.#waterAmount)  // Error: can only access from Fruit
+  }
+}
