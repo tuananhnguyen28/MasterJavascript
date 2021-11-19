c9e1694d491b564dde3456e73894ee1d321c49f1
tuananhnguyen28 <tuananhnguyenasp@gmail.com>
2021-11-19 20:34:36 +0700

    Clear input form after adding succeed / Add each item to UI by using LocalStorage

--- app.js
+++ app.js
@@ -22,6 +22,33 @@ UI.prototype.add = function (student) {
     </td>
   `
   document.querySelector('table tbody').appendChild(tr)
+
+  // Clear form data after adding succeed
+  document.getElementById('name').value = ''
+  document.getElementById('birthday').value = ''
+}
+
+// Render all data (after loading completely all HTML coding)
+UI.prototype.renderAll = function() {
+  const store = new Store()
+  const students = store.getStudents()
+  let html = students.reduce((result, current, currentIndex) => {
+    return result = `
+      <tr>
+        <td>${currentIndex + 1}</td>
+        <td>${current.name}</td>
+        <td>${current.birthday}</td>
+        <td>
+          <button class="btn btn-sm btn-danger btn-remove" data-id="${current.id}">Xóa</button>
+        </td>
+      </tr>
+    `
+  }, '')
+  document.querySelector('table tbody').innerHTML = html
+}
+
+UI.prototype.clear = function() {
+  document.querySelector('table tbody').innerHTML = ''
 }
 
 // Hàm trả về danh sách các students
@@ -46,4 +73,10 @@ document.querySelector('form').addEventListener('submit', event => {
   const student = new Student(name, birthday)
   ui.add(student)
   store.add(student)
+})
+
+// Sau khi load hết HTML, sẽ chạy hàm này để add từng phần tử vào UI, không mất khi reload page
+window.addEventListener('DOMContentLoaded', () => {
+  const ui = new UI()
+  ui.renderAll()
 })
