a12367f85d7e60d3674ae039330c0ed201905509
tuananhnguyen28 <tuananhnguyenasp@gmail.com>
2021-12-07 22:30:37 +0700

    Json, Ajax & Fetch and how to using JSON and definitions about JSON as well

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MasterJavascript
+# Khóa học Master Javascript - Cơ bản đến nâng cao - Dư Thanh Được
+
+# Hướng Dẫn 9 | JSON, Ajax và Fetch API
+  # Lesson 1: Json, Ajax & Fetch
+  # Facebook: https://www.facebook.com/groups/495362234941379/learning_content/?filter=185126270096765&post=470606764146557
+  # Youtube: https://youtu.be/aLk9IKrBukc
---
+++ app.js
@@ -0,0 +1,48 @@
+/*
+  Định nghĩa: JSON (Javascript Object Notation) là một chuỗi text được viết dưới dạng Javascript Object dùng
+  để lưu trữ và trao đổi dữ liệu.
+*/
+
+// 1. Chuyển Object thành JSON
+const object1 = {
+  name: 'Tuan',
+  age: 28,
+  cars: ['Mercedes', 'Lamborghini', 'BMW']
+}
+const jsonResult = JSON.stringify(object1)
+console.log('Chuyển Object -> JSON:', jsonResult)
+
+// 2. Chuyển JSON sang Object
+const json1 = `{"name":"Tuan","age":28,"cars":["Mercedes","Lamborghini","BMW"]}`
+const objectResult = JSON.parse(json1)
+console.log('Chuyển JSON -> Object:', objectResult)
+
+// 3. Array và JSON
+const originArray = ['A', 'B', 'C']
+const jsonOfArray = '["A","B","C"]' // hoặc `["A","B","C"]`
+console.log('----- Array và JSON -----')
+console.log(jsonOfArray === JSON.stringify(originArray)) // true
+
+// 4. Number và JSON
+const jsonOfNumber = JSON.stringify(1)
+console.log('----- Number và JSON -----')
+console.log(jsonOfNumber === '1') // true
+
+// 5. String và JSON
+const jsonOfString = JSON.stringify('tuan')
+console.log('----- String và JSON -----')
+console.log(jsonOfString === '"tuan"')  // true
+
+// 6. Boolean và JSON
+const jsonOfBoolean = JSON.stringify(true)
+console.log('----- Boolean và JSON -----')
+console.log(jsonOfBoolean === 'true') // true
+
+// 7. Ngoài các giá trị trên thì các giá trị như Function, undefined, NaN, Infinity ... sẽ không chuyển sang được JSON
+const jsonContainsFunction = {
+  name: 'Tuan',
+  age: 28,
+  eat: function() { console.log(`The ${name} is eating!`)}
+}
+const jsonContainsFunction_Result = JSON.stringify(jsonContainsFunction)
+console.log('JSON chứa function không thể chuyển sang JSON:', jsonContainsFunction_Result)
