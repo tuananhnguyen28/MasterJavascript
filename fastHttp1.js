1fc722d4d107c71aaa72f3770bd9e15d0a0883cd
tuananhnguyen28 <tuananhnguyenasp@gmail.com>
2022-01-06 22:24:42 +0700

    Initialize Post/Put/Delete methods and refactor the previous coding

--- fastHttp1.js
+++ fastHttp1.js
@@ -5,7 +5,7 @@ function FastHttp() {
   this.xhr = new XMLHttpRequest()
 }
 
-FastHttp.prototype.get = function(url, callback) {
+FastHttp.prototype.send = function(method, url, body, callback) {
   this.xhr.onreadystatechange = function() {
     if(this.readyState === 4) {
       if(this.status === 200 || this.status === 201) {
@@ -15,12 +15,30 @@ FastHttp.prototype.get = function(url, callback) {
       }
     }
   }
-  this.xhr.open('GET', url, true)
-  this.xhr.send()
+  this.xhr.open(method, url, true)
+  this.xhr.setRequestHeader('Content-Type', 'application/json')
+  this.xhr.send(body ? JSON.stringify(body) : null)
+}
+
+FastHttp.prototype.get = function(url, callback) {
+  this.send('GET', url, null, callback)
+}
+
+FastHttp.prototype.post = function(url, body, callback) {
+  this.send('POST', url, body, callback)
+}
+
+FastHttp.prototype.put = function(url, body, callback) {
+  this.send('PUT', url, body, callback)
+}
+
+FastHttp.prototype.delete = function(url, callback) {
+  this.send('DELETE', url, null, callback)
 }
 
 const http = new FastHttp()
 const baseURL = 'https://61d5a36e2b4f730017a82974.mockapi.io/users'
+
 http.get(baseURL, (error, data) => {
   if(error) {
     console.log('Error', error)
@@ -28,3 +46,27 @@ http.get(baseURL, (error, data) => {
     console.log(data)
   }
 })
+
+// http.post(baseURL, { name: 'Tran Minh Khoa' } , (error, data) => {
+//   if(error) { 
+//     console.log('Error', error)
+//   } else {
+//     console.log(data)
+//   }
+// })
+
+// http.put(`${baseURL}/6`, { name: 'Tuan-updated' }, (error, data) => {
+//   if(error) { 
+//     console.log('Error', error)
+//   } else {
+//     console.log(data)
+//   }
+// })
+
+// http.delete(`${baseURL}/6`, (error, data) => {
+//   if(error) { 
+//     console.log('Error', error)
+//   } else {
+//     console.log(data)
+//   }
+// })
