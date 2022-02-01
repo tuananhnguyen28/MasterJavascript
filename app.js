c2107fca8add95561db23d3b57cdc680445d6c34
tuananhnguyen28 <tuananhnguyenasp@gmail.com>
2022-02-01 23:10:42 +0700

    Destructuring with array and Rest parameter

--- app.js
+++ app.js
@@ -61,3 +61,46 @@ console.log('eployee ban đầu')
 console.log(employee) // tất cả các thuộc tính cũ không thay đổi giá trị
 console.log('employee sau khi clone')
 console.log(employeeClone)  // thuộc tính mới sẽ ghi đè thuộc tính object cũ
+
+// Đối với Array
+const list1 = [1, 2, 3, 4]
+const list2 = [5, 6, 7, 8]
+const list3 = [...list1, ...list2]
+const list3_Swap = [...list2, ...list1]
+console.log(`list3: ${list3}`)
+console.log(`list3_Swap: ${list3_Swap}`)
+
+// Dùng Shallow copy đối với array
+cloneList1 = [...list1]
+console.log(list1 === cloneList1) // false
+
+// Thêm một phần tử đối với array
+const list4 = [...list1, 10]
+console.log(`list4 sau khi thêm số 10 và clone từ list1: ${list4}`)
+
+
+// 2. Rest Parameter
+
+// Kết hợp destructuring và rest parameter để lấy các giá trị còn lại của một phần tử
+const handle2 = ({ a, b, ...c }) => {
+  console.log('Các giá trị còn lại của c (đối với object):')
+  console.log(c)
+}
+handle2({ a: 1, b: 2, c: 3, d: 4, e:5 })  // {c: 3, d: 4, e: 5}
+
+// Vẫn áp dụng với array
+const handle_array = ([a, b, c, ...d]) => {
+  console.log('Các giá trị còn lại của d (đối với mảng):')
+  console.log(d)
+}
+handle_array([1, 2, 3, 4, 5, 6, 7, 8])  // [4, 5, 6, 7, 8]
+
+// Ví dụ khác (dùng Rest Parameter để split ra các thuộc tính muốn store trong 1 object)
+const cake = {
+  name: 'Birthday Cake',
+  price: 59,
+  quantity: 1
+}
+const { name, ...other } = cake
+console.log('Object chứa các thuộc tính sau khi dùng Rest Parameter để split:')
+console.log(other)
