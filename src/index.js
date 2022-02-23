36d139c3fdbc387be6003fd7dc5dda9459b7e1bb
tuananhnguyen28 <tuananhnguyenasp@gmail.com>
2022-02-23 21:12:12 +0700

    Handle render all data by arrow function and http method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import http from './http'
 import 'bootstrap/scss/bootstrap.scss'
 
-http.getPosts().then(res => {
-  console.log(res)
-})
+const renderAll = () => [
+  http.getPosts().then(res => {
+    const listNode = document.getElementById('list')
+    listNode.innerHTML = ''
+    const html = res.reduce((result, current) => {
+      result += `
+        <div class="card flex-row align-items-center justify-content-between p-2 mb-3">
+        <div>
+          <p><strong>${current.title}</strong></p>
+          <p>${current.description}</p>
+        </div>  
+        <div>
+          <button class="btn btn-info" id="btn-start-edit" data-id="${current.id}">Edit</button>
+          <button class="btn btn-danger" id="btn-remove"  data-id="${current.id}">Remove</button>
+        </div>
+      </div>
+      `
+      return result
+    }, '')
+    listNode.innerHTML = html
+  })
+]
+
+const init = () => {
+
+  // Render All
+  renderAll()
+
+}
+
+window.addEventListener('DOMContentLoaded', init) 
